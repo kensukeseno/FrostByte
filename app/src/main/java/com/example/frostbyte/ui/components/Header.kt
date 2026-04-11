42249f671b74953eba32cdebd7f4414b2a3e5ef5
ken <ks23.cn.gyz@gmail.com>
2026-04-11 13:13:25 -0600

    Added back button

--- app/src/main/java/com/example/frostbyte/ui/components/Header.kt
+++ app/src/main/java/com/example/frostbyte/ui/components/Header.kt
@@ -1,5 +1,6 @@
 package com.example.frostbyte.ui.components
 
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -7,11 +8,19 @@ import androidx.navigation.NavController
 
 @Composable
 fun Header(navController: NavController, title: String) {
-    Button(
-        onClick = { navController.navigate("home") }
-    ) {
-        Text("Logo")
-    }
+    Row {
+        Button(
+            onClick = { navController.navigate("home") }
+        ) {
+            Text("Logo")
+        }
 
-    Text(text = title)
+        Button(
+            onClick = { navController.popBackStack() }
+        ) {
+            Text("Back")
+        }
+
+        Text(text = title)
+    }
 }
